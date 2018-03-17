ac41c2f2086462b1432d884c53a3e6279c994bc3
Joël Quenneville <joelq@thoughtbot.com>
2018-03-17 13:27:42 -0400

    Initialize project with app skeleton

---
+++ Main.elm
@@ -0,0 +1,41 @@
+module Main exposing (main)
+
+import Html exposing (Html)
+
+
+type alias Model =
+    {}
+
+
+type Msg
+    = Noop
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+view : Model -> Html a
+view model =
+    Html.text "hello world"
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
