17cdf97af7cbe47cb09fec123195463c9cfb1ca8
Joël Quenneville <joelq@thoughtbot.com>
2018-04-04 22:09:14 -0400

    basic generator

--- River/Random.elm
+++ River/Random.elm
@@ -1,28 +1,78 @@
-module River.Random exposing (allowedNextBlocks)
+module River.Random exposing (section)
 
-import River exposing (Block(..))
+import Section exposing (Section, ObstacleArrangement(..), Savepoint(..))
+import Random exposing (Generator)
+import Random.Extra
 
 
-allowedNextBlocks : Block -> List Block
-allowedNextBlocks block =
-    case block of
+section : ObstacleArrangement -> Generator Section
+section arr =
+    Random.Extra.constant Section
+        |> Random.andThen (\f -> Random.map f savepoint)
+        |> Random.andThen (\f -> Random.map (\obs -> ( obs, f obs )) <| obstacleArrangement arr)
+        |> Random.andThen (\( prev, f ) -> Random.map (\obs -> ( obs, f obs )) <| obstacleArrangement prev)
+        |> Random.andThen (\( prev, f ) -> Random.map (\obs -> ( obs, f obs )) <| obstacleArrangement prev)
+        |> Random.andThen (\( prev, f ) -> Random.map (\list -> ( List.head list |> Maybe.withDefault prev, f list )) <| obstacleList prev)
+        |> Random.andThen (\( prev, f ) -> Random.map (\obs -> ( obs, f obs )) <| obstacleArrangement prev)
+        |> Random.map Tuple.second
+
+
+obstacleList : ObstacleArrangement -> Generator (List ObstacleArrangement)
+obstacleList arr =
+    nextObstacleInList arr (Random.Extra.constant [])
+
+
+nextObstacleInList :
+    ObstacleArrangement
+    -> Generator (List ObstacleArrangement)
+    -> Generator (List ObstacleArrangement)
+nextObstacleInList arr gen =
+    Random.Extra.maybe Random.bool (obstacleArrangement arr)
+        |> Random.andThen
+            (\maybe ->
+                case maybe of
+                    Just this ->
+                        nextObstacleInList this (Random.map (\list -> this :: list) gen)
+
+                    Nothing ->
+                        gen
+            )
+
+
+savepoint : Generator Savepoint
+savepoint =
+    Random.Extra.sample [ TopWolf, BottomWolf, NoWolf ]
+        |> Random.map (Maybe.withDefault NoWolf)
+
+
+obstacleArrangement : ObstacleArrangement -> Generator ObstacleArrangement
+obstacleArrangement arr =
+    arr
+        |> allowedNextBlocks
+        |> Random.Extra.sample
+        |> Random.map (Maybe.withDefault ClearWater)
+
+
+allowedNextBlocks : ObstacleArrangement -> List ObstacleArrangement
+allowedNextBlocks arr =
+    case arr of
         TwoTop ->
-            [ TwoTop, OneTop, OneMiddle, NoBlocks ]
+            [ TwoTop, OneTop, OneMiddle, ClearWater ]
 
         TwoBottom ->
-            [ TwoBottom, OneBottom, OneMiddle, NoBlocks ]
+            [ TwoBottom, OneBottom, OneMiddle, ClearWater ]
 
         OneTop ->
-            [ TwoTop, OneTop, OneBottom, OneMiddle, TopAndBottom, NoBlocks ]
+            [ TwoTop, OneTop, OneBottom, OneMiddle, TopAndBottom, ClearWater ]
 
         OneBottom ->
-            [ TwoBottom, OneTop, OneBottom, OneMiddle, TopAndBottom, NoBlocks ]
+            [ TwoBottom, OneTop, OneBottom, OneMiddle, TopAndBottom, ClearWater ]
 
         OneMiddle ->
-            [ OneMiddle, NoBlocks ]
+            [ OneMiddle, ClearWater ]
 
         TopAndBottom ->
-            [ OneTop, OneBottom, TopAndBottom, NoBlocks ]
+            [ OneTop, OneBottom, TopAndBottom, ClearWater ]
 
-        NoBlocks ->
-            [ TwoTop, TwoBottom, OneTop, OneBottom, OneMiddle, TopAndBottom, NoBlocks ]
+        ClearWater ->
+            [ TwoTop, TwoBottom, OneTop, OneBottom, OneMiddle, TopAndBottom, ClearWater ]
